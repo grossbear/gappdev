4493a63f5b0285d1970709381ac59cc2233ba763
Michal Lichojwan <michal.lichojwan@edge1s.com>
2017-12-04 17:50:27 +0100

    All MathPrim function from prev version
    Add in MathPrim source file all functions from previous version of the
    project. Removed unnecessary template functions from previous project.

--- Research/MathLibTest/mathlibtest.cpp
+++ Research/MathLibTest/mathlibtest.cpp
@@ -15,5 +15,18 @@ int main()
     float fval = mitof(ival);
     printf("converted fval = %.6f\n",fval);
 
+    float cval = 0.5f;
+    float packedval = mpack01(cval);
+    printf("packed value 01: %.6f\n",packedval);
+    
+    float fval1 = -0.25f;
+    fval1 = mabs(fval1);
+    
+    int32t ival1 = -4;
+    ival1 = mabs(ival1);
+    
+    float fval2 = mclamp(3.f,6.f,1.f);
+    int ival2 = mclamp(-3,9,2);
+
     return 0;
 }
