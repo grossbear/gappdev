9f444f8413d750558a43308aa010ef16944e72a3
Michal Lichojwan <mlichojwan@vewd.com>
2018-05-23 17:02:29 +0200

    Add half precision float type class

--- Framework/Base/Math/Types/thalf.h
+++ Framework/Base/Math/Types/thalf.h
@@ -217,4 +217,60 @@ bool mpinf(uint16t h);
 // Is Negative Infinity
 bool mninf(uint16t h);
 
+///////////////////////////////////////////////////////////////////////////////////////
+// Math 16-bit half precision floating point type class
+class thalf
+{
+protected:
+    uint16t value;
+
+public:
+    // Constructors Declarations
+    thalf();
+    thalf(const thalf &half_val);
+    thalf(float f_val);
+    thalf(double d_val);
+    thalf(int32t i_val);
+    thalf(int16t i_val);
+
+    // Cast To Pointer uint16t *
+    operator uint16t* () const;
+    operator const uint16t* () const;
+
+    // Cast To Promitive Built-in Types
+    operator float() const;
+    operator double() const;
+    operator int32t() const;
+    operator int16t() const;
+    operator bool() const;
+
+    // Assignment Operators
+    thalf& operator = (const thalf& half_val);
+    thalf& operator +=(const thalf& half_val);
+    thalf& operator -=(const thalf& half_val);
+    thalf& operator *=(const thalf& half_val);
+    thalf& operator /=(const thalf& half_val);
+
+    // Unary Operators
+    thalf operator + () const;
+    thalf operator - () const;
+
+    // Binary Operators
+    thalf operator + (const thalf &half_val) const;
+    thalf operator - (const thalf &half_val) const;
+    thalf operator * (const thalf &half_val) const;
+    thalf operator / (const thalf &half_val) const;
+
+    bool operator == (const thalf &half_val) const;
+    bool operator != (const thalf &half_val) const;
+    
+    bool operator <  (const thalf &half_val) const;
+    bool operator >  (const thalf &half_val) const;
+    
+    bool operator <= (const thalf &half_val) const;
+    bool operator >= (const thalf &half_val) const;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+
 #endif //_HALF_H_
